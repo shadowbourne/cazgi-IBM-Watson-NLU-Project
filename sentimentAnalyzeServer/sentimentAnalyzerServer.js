08591d118384bed9bf407daab919ae52ef85b9d1
Max <shadowbourne3@gmail.com>
2021-06-29 21:43:24 +0000

    stage 3

--- sentimentAnalyzeServer/sentimentAnalyzerServer.js
+++ sentimentAnalyzeServer/sentimentAnalyzerServer.js
@@ -1,5 +1,25 @@
 const express = require('express');
+const dotenv = require('dotenv');
 const app = new express();
+dotenv.config();
+
+function getNLUInstance() {
+  let api_key = process.env.API_KEY;
+  let api_url = process.env.API_URL;
+  
+  const NaturalLanguageUnderstandingV1 = require("ibm-watson/natural-language-understanding/v1");
+  const { IamAuthenticator } = require(ibm-watson/auth);
+  const naturalLanguageUnderstanding = new NaturalLanguageUnderstandingV1({
+      version: "2020-08-01",
+      authenticator: new IamAuthenticator({
+        apikey: api_key,
+      }),
+      serviceUrl: api_url,
+    });
+    
+      
+  };
+
 
 app.use(express.static('client'))
 
